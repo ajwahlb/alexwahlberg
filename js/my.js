d1b886954150320d688a8a53f3756f15940332c1
Alex Wahlberg <alex.wahlberg@egov.com>
2019-03-13 09:08:40 -0500

    filter

--- js/my.js
+++ js/my.js
@@ -8,3 +8,24 @@ $('.btn-nav, .nav-close').click(function(){
 	$('.slideout-menu').toggleClass("navtoggle");
 	$('.slideout-menu').delay( 300 ).toggleClass("slideout-open");
 });
+
+// filter
+$('#filter li.fl').click(function(){
+	$('#thumbnails .project').hide();
+	$('#thumbnails .project.logo').show();
+});
+$('#filter li.fp').click(function(){
+	$('#thumbnails .project').hide();
+	$('#thumbnails .project.painting').show();
+});
+$('#filter li.fi').click(function(){
+	$('#thumbnails .project').hide();
+	$('#thumbnails .project.illustration').show();
+});
+$('#filter li.fw').click(function(){
+	$('#thumbnails .project').hide();
+	$('#thumbnails .project.web').show();
+});
+$('#filter li.fa').click(function(){
+	$('#thumbnails .project').show();
+});
