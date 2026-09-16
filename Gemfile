9b5405396cf2d62d64da6b1d7573f8016fdc9916
Alex Wahlberg <alexjwahlberg@gmail.com>
2026-09-15 21:07:54 -0500

    first round updates

--- Gemfile
+++ Gemfile
@@ -8,7 +8,8 @@ source "https://rubygems.org"
 #
 # This will help ensure the proper Jekyll version is running.
 # Happy Jekylling!
-gem "jekyll", "~> 3.6.0"
+
+gem "jekyll", "~> 4.3"
 
 # This is the default theme for new Jekyll sites. You may change this to anything you like.
 gem "minima", "~> 2.0"
@@ -25,3 +26,16 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
+gem "base64", "~> 0.3.0"
+
+gem "bigdecimal", "~> 4.1"
+
+gem "rexml", "~> 3.4"
+
+gem "webrick", "~> 1.9"
+
+gem "csv"
+
+gem "logger"
+
+gem "jekyll-sass-converter", "~> 2.2"
--- js/my.js
+++ js/my.js
@@ -1,25 +1,9 @@
+const emailBtn = document.getElementById("email-btn");
 
-
-// if ($(window).width() < 768) {
-// 	$('.new-break').replaceWith('<br>');
-// }
-$('.btn-nav, .nav-close').click(function(){
-	console.log("nav clicked");
-	$('.slideout-menu').toggleClass("navtoggle");
-	$('.slideout-menu').delay( 300 ).toggleClass("slideout-open");
-});
-
-// filter
-$('#filter li').click(function() {
-	if ($(this).hasClass('all')){
-		$('#filter li').removeClass('selected');
-		$(this).addClass('selected');
-		$('#thumbnails .project').show();
-	} else {
-		var filterWork = $(this).data('filter');
-		$('#filter li').removeClass('selected');
-		$('#thumbnails .project').hide();
-		$(this).addClass('selected');
-		$('#thumbnails .project[data-filter=' + filterWork + ']').show();
-	}
+emailBtn.addEventListener("click", function () {
+  const user = "yourname";
+  const domain = "example.com";
+  
+  // Set mailto href right before user clicks
+  this.href = `mailto:${user}@${domain}`;
 });
